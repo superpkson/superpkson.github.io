0cae8911e8151a71c077c5d5e674cbee0e542270
superpkson <superpkson@gmail.com>
2026-01-29 02:33:45 +0800

    added chart

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,5 +1,65 @@
-$(document).ready(function () {
-	const ctx = document.getElementById('myChart');
+$(document).ready(function () {	
+	const servers = JSON.parse(getCookie('servers'));
+	servers.forEach(server => {
+		addCard(server);
+	});
+});
+
+$(function() {	
+	$("#btn-add-server").click(function() {
+		let name = prompt('Server name: ', 'computer');
+		let addr = prompt('Server address: ', 'wss://');
+		
+		servers = JSON.parse(getCookie('servers'));
+		if (servers === null)
+			servers = [];
+		servers.push({
+			name: name,
+			addr: addr
+		});
+		
+		setCookie('servers', JSON.stringify(servers))
+	});
+});
+
+
+function setCookie(cname, cvalue) {
+	document.cookie = cname + "=" + cvalue + "; path=/; SameSite=Lax; secure";
+}
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return null;
+}
+
+function addCard(obj) {
+	content = `<div class="col-sm-12 mt-1">
+				<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">` + obj.name + `</h5>
+						<h6 class="card-subtitle mb-2 text-muted">` + obj.addr + `</h6>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="card-link">Card link</a>
+						<a href="#" class="card-link">Another link</a>
+						<canvas id="` + obj.name + `-gpu"></canvas>
+						<canvas id="` + obj.name + `-memory"></canvas>
+					</div>
+				</div>
+			</div>`;
+	$("#list-cards").append(content);
+	
+	const ctx = document.getElementById(obj.name + "-gpu");
 	
 	const annotation = {
 	  type: 'doughnutLabel',
@@ -56,124 +116,40 @@ $(document).ready(function () {
 
 	const chart = new Chart(ctx, config);
 	
-	
-	const ctx2 = document.getElementById('myChart2');
-	
-	const DATA_COUNT = 5;
-	const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
-
-	const data2 = {
-	  labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
-	  datasets: [
-		{
-		  label: 'Dataset 1',
-		  data: [5, 89, 36, 34, 14],
-		  backgroundColor: Object.values({
-  red: 'rgb(255, 99, 132)',
-  orange: 'rgb(255, 159, 64)',
-  yellow: 'rgb(255, 205, 86)',
-  green: 'rgb(75, 192, 192)',
-  blue: 'rgb(54, 162, 235)',
-  purple: 'rgb(153, 102, 255)',
-  grey: 'rgb(201, 203, 207)'
-}),
-		}
-	  ]
-	};
-	
-	const config2 = {
-	  type: 'doughnut',
-	  data: data2,
-	  options: {
-		responsive: false,
-		plugins: {
-		  legend: {
-			position: 'top',
-		  },
-		  title: {
-			display: true,
-			text: 'Chart.js Doughnut Chart'
-		  }
-		}
-	  },
-	};
-
-	new Chart(ctx2, config2);
-	
-	function connect(uri){
-		var ws = new WebSocket(uri);
-		
-		ws.onopen = function(event) {
-			console.log('Connection established.');
-			// You can send a message here upon open
-		};
-		
-		ws.onmessage = function(event) {
-			// console.log('Message received:', event.data);
-			// Process the incoming data
-			const obj = JSON.parse(event.data)
-			console.log(obj)
-			
-			chart.data.datasets.forEach(dataset => {
-				dataset.data = [obj[0].temp, 75 - obj[0].temp];
-			});
-			chart.update();
-		};
-		
-		ws.onclose = function(event) {
-			// console.log('Connection closed:', event);
-			setTimeout(connect(uri), 10000);
-		};
-		
-		ws.onerror = function(error) {
-			// console.error('WebSocket error:', error);
-		};
-	}
-	
-	const servers = JSON.parse(getCookie('servers'));
-	servers.forEach(server => {
-		connect(server.addr);
-	});
-});
+	connect(chart, obj.addr);
+}
 
 function index(perc) {
   return perc < 70 ? 0 : perc < 90 ? 1 : 2;
 }
 
-$(function() {	
-	$("#btn-add-server").click(function() {
-		let name = prompt('Server name: ', 'computer');
-		let addr = prompt('Server address: ', 'wss://');
+
+function connect(chart, uri){
+	var ws = new WebSocket(uri);
+	
+	ws.onopen = function(event) {
+		console.log('Connection established.');
+		// You can send a message here upon open
+	};
+	
+	ws.onmessage = function(event) {
+		// console.log('Message received:', event.data);
+		// Process the incoming data
+		const obj = JSON.parse(event.data)
+		// console.log(obj)
 		
-		servers = JSON.parse(getCookie('servers'));
-		if (servers === null)
-			servers = [];
-		servers.push({
-			name: name,
-			addr: addr
+		chart.data.datasets.forEach(dataset => {
+			dataset.data = [obj[0].temp, 75 - obj[0].temp];
 		});
-		
-		setCookie('servers', JSON.stringify(servers))
-	});
-});
-
-
-function setCookie(cname, cvalue) {
-	document.cookie = cname + "=" + cvalue + "; path=/; SameSite=Lax; secure";
-}
-
-function getCookie(cname) {
-	let name = cname + "=";
-	let decodedCookie = decodeURIComponent(document.cookie);
-	let ca = decodedCookie.split(';');
-	for(let i = 0; i <ca.length; i++) {
-		let c = ca[i];
-		while (c.charAt(0) == ' ') {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-			return c.substring(name.length, c.length);
-		}
-	}
-	return null;
+		chart.update();
+	};
+	
+	ws.onclose = function(event) {
+		// console.log('Connection closed:', event);
+		setTimeout(connect(uri), 10000);
+	};
+	
+	ws.onerror = function(error) {
+		// console.error('WebSocket error:', error);
+	};
 }
